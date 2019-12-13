35a8e5948058d4554f50ed52de12c87ab2b684c5
ToJaJestemStaszek <sstanislaw85@gmail.com>
2019-12-13 06:56:26 +0100

    Added testing

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
-import Spinner from '../../components/UI/Spinner/Spinner'
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
-import {Redirect} from 'react-router-dom';
 import * as actions from '../../store/actions/index';
-import {updateObject, checkValidity} from '../../shared/utility'
+import { updateObject, checkValidity } from '../../shared/utility';
 
 class Auth extends Component {
     state = {
@@ -44,25 +44,20 @@ class Auth extends Component {
         isSignup: true
     }
 
-    componentDidMount() {
-
-        if(!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
-            this.props.onSetAuthRedirectPath()
+    componentDidMount () {
+        if ( !this.props.buildingBurger && this.props.authRedirectPath !== '/' ) {
+            this.props.onSetAuthRedirectPath();
         }
-
     }
 
-    
-
     inputChangedHandler = ( event, controlName ) => {
-        const updatedControls = updateObject(this.state.controls, {
-            [controlName]: updateObject(this.state.controls[controlName], {
+        const updatedControls = updateObject( this.state.controls, {
+            [controlName]: updateObject( this.state.controls[controlName], {
                 value: event.target.value,
                 valid: checkValidity( event.target.value, this.state.controls[controlName].validation ),
                 touched: true
-            }) 
-        }) 
-        
+            } )
+        } );
         this.setState( { controls: updatedControls } );
     }
 
@@ -72,9 +67,9 @@ class Auth extends Component {
     }
 
     switchAuthModeHandler = () => {
-        this.setState(prevState => {
-            return {isSignup: !prevState.isSignup};
-        });
+        this.setState( prevState => {
+            return { isSignup: !prevState.isSignup };
+        } );
     }
 
     render () {
@@ -98,20 +93,21 @@ class Auth extends Component {
                 changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
         ) );
 
-        if(this.props.loading) {
+        if ( this.props.loading ) {
             form = <Spinner />
         }
 
         let errorMessage = null;
 
-        if(this.props.error) {
+        if ( this.props.error ) {
             errorMessage = (
                 <p>{this.props.error.message}</p>
-            )
+            );
         }
+
         let authRedirect = null;
-        if(this.props.isAuthenticated) {
-         authRedirect  = <Redirect to = {this.props.authRedirectPath} />
+        if ( this.props.isAuthenticated ) {
+            authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
@@ -122,7 +118,7 @@ class Auth extends Component {
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
-                <Button 
+                <Button
                     clicked={this.switchAuthModeHandler}
                     btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
             </div>
@@ -137,14 +133,13 @@ const mapStateToProps = state => {
         isAuthenticated: state.auth.token !== null,
         buildingBurger: state.burgerBuilder.building,
         authRedirectPath: state.auth.authRedirectPath
-        
-    }
-}
+    };
+};
 
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
-        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
+        onSetAuthRedirectPath: () => dispatch( actions.setAuthRedirectPath( '/' ) )
     };
 };
 
